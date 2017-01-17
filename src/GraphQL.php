5f34de042370cd27f99be66a4de4a3af5c3c561c
Grummfy <me@grummfy.be>
2017-01-17 12:09:32 +0100

    try & errors

---
+++ src/GraphQL.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+// 1. Load grammar.
+$compiler = Hoa\Compiler\Llk\Llk::load(new Hoa\File\Read(__DIR__ . '/GraphQL.pp'));
+
+// 2. Parse a data.
+$ast      = $compiler->parse(file_get_contents(__DIR__ . '/data.txt'));
+
+// 3. Dump the AST.
+$dump     = new Hoa\Compiler\Visitor\Dump();
+echo $dump->visit($ast);
